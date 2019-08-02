27dd2b995862cff537698efce7cd4d8169d28be7
董瑞鑫 <957251869@qq.com>
2019-08-02 19:24:57 +0800

    pppig_parent

---
+++ pppig_parent/pppig_common/src/main/java/entity/PageResult.java
@@ -0,0 +1,34 @@
+package entity;
+
+import java.util.List;
+
+/**
+ * 分页结果类
+* @param <T>
+*/
+public class PageResult<T> {
+    private Long total;
+    private List<T> rows;
+    public PageResult(Long total, List<T> rows) {
+        super();
+        this.total = total;
+        this.rows = rows;
+    }
+    //getter and setter ....
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<T> rows) {
+        this.rows = rows;
+    }
+}
---
+++ pppig_parent/pppig_user/src/main/java/com/pppig/user/UserApplication.java
@@ -0,0 +1,32 @@
+package com.pppig.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import utils.IdWorker;
+import utils.JwtUtil;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+	@Bean
+	public IdWorker idWorkker(){
+		return new IdWorker(1, 1);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bcryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+	@Bean
+	public JwtUtil gitJwtUtil(){
+		return new utils.JwtUtil();
+	}
+}
